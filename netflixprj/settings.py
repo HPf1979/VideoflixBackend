8ad7a5a3cb3324407d20853844fba29ded1b9ab5
Herlina Pfeiffer <pfeiffer.herlina@gmx.net>
2023-11-08 23:02:43 +0100

    cors adjusted

--- netflixprj/settings.py
+++ netflixprj/settings.py
@@ -150,7 +150,8 @@ REST_FRAMEWORK = {
 }
 
 # LOGIN_REDIRECT_URL ='/'
-# CORS_ALLOWED_ORIGINS = [
-#   "http://localhost:4200",
-#   "http://127.0.0.1:4200",]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://herlina-pfeiffer.developerakademie.net",
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",]
+# CORS_ALLOW_ALL_ORIGINS = True
