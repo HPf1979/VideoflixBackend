5c1da384b4e641c7c8204642c9d97bdd989c08c2
Herlina Pfeiffer <pfeiffer.herlina@gmx.net>
2023-11-12 08:22:35 +0000

    postgresDataBase integrated

--- netflixprj/settings.py
+++ netflixprj/settings.py
@@ -93,10 +93,21 @@ WSGI_APPLICATION = 'netflixprj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'videoflixdatabase',   # Der Name deiner PostgreSQL-Datenbank
+        'USER': 'herlina',             # Dein PostgreSQL-Benutzername
+        'PASSWORD': 'newdatabase',  # Dein PostgreSQL-Passwort
+        'HOST': 'localhost',           # Oder die Adresse deiner PostgreSQL-Datenbank
+        'PORT': '5432',                # Standard-Port für PostgreSQL
     }
 }
 
